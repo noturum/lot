51e13601ab85065faf74a48f45ab41bdbdf3dd29
noturum <34236299+noturum@users.noreply.github.com>
2024-01-11 15:53:57 +0700

    11.01
    com

--- app.py
+++ app.py
@@ -4,22 +4,83 @@ from telebot import TeleBot
 from DbController import User,Group, Database as db
 import logging
 import pickle
+import re
 from threading import Thread,Event
 logging.basicConfig(filename='/root/bot/error.log',
                     format='[%(asctime)s] => %(message)s',
                     level=logging.ERROR)
-import bs4,lxml,requests
+import lxml,requests
+from bs4 import BeautifulSoup as bs
 bot = TeleBot(API_KEY)
-
+from selenium.webdriver import Chrome,ChromeOptions,DesiredCapabilities
+from selenium.common import SessionNotCreatedException
 
 class LinkScraber(Thread):
-    def __init__(self):
-        
-        super().__init__(daemon=True)
+    MAIN='https://www.youtube.com'
+    LINK_THREDS='https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl'
+    options = ChromeOptions()
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    options.add_experimental_option("prefs", prefs)
+    options.add_argument('log-level=3')
+    caps = DesiredCapabilities().CHROME
+    caps["pageLoadStrategy"] = "none"
 
+    def __init__(self, headless=False):
+        super().__init__(daemon=True)
+        self._driver = self._get_driver(headless)
+        self._windows = {}
+        self.links=[]
+
+    def _get_driver(self, headless: bool = False):
+        if headless == True:
+            self.options.add_argument('--headless')
+            self.options.add_argument('--disable-gpu')
+        try:
+            return Chrome(options=self.options)
+        except SessionNotCreatedException as e:
+            print(e)
+            exit(1)
+
+    def load(self, url, tab_name='main'):
+        if tab_name in self._windows.keys():
+            self._driver.switch_to.window(self._windows[tab_name])
+        else:
+            self._driver.switch_to.new_window('tab')
+            self._windows[tab_name] = self._driver.window_handles[-1]
+        self._driver.get(url)
+
+    def _get_text(self, tab_name=None):
+        if tab_name:
+            self._driver.switch_to.window(self._windows[tab_name])
+        return self._driver.page_source
+    def get_threds(self):
+        self.load(self.LINK_THREDS)
+        soup = bs(self.get_text(), 'lxml')
+        links = soup.find_all('a')
+        threds=[]
+        for link in links:
+            if (hr := link.get('href')) and hr not in threds:
+                threds.append(hr)
+        return threds
+    def _rem_dubl(self,lst):
+        for ind, i in enumerate(lst):
+            if lst.count(i) > 1:
+                lst.pop(ind)
+
+        super().__init__(daemon=True)
+    def _get_tg(self,url):
+
+        try:
+            links = re.findall("https:\/\/t.me[\w@^\/]*",
+                           requests.get(url).text)
+            self._rem_dubl(links)
+            print(links)
+        except BaseException:
+            return None
     def run(self):
-        self.client.get(link)
-        ...
+        for thred in self.get_threds():
+            self._get_tg(self.MAIN+thred)
+
 
 
 
